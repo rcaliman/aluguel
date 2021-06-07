16380fd03735b1dfbfe2511ed4b715b3992e1954
Rodrigo Caliman <rcaliman@gmail.com>
2021-06-07 09:36:25 -0300

    retirando parametros

--- parametros_banco.py
+++
@@ -1,8 +0,0 @@
-def parametros():
-   return   {   
-                'host':'localhost',
-                'database':'aluguel',
-                'user':'aluguel',
-                'password':'4lugu3l',
-                'auth_plugin':'mysql_native_password',
-            }
