ab02901f90356bd1f9afe54901f3bf3e09244002
Rodrigo Caliman <rcaliman@gmail.com>
2022-06-06 23:30:44 +0000

    atualizacao feita do servidor

---
+++ parametros_banco.py
@@ -0,0 +1,8 @@
+def parametros():
+	return	{
+				'database':'aluguel',
+               	'user':'aluguel',
+                'password':'4lugu3l',
+                'auth_plugin':'mysql_native_password',
+            }
+				
